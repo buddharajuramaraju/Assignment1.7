05a41a60cdc3b49126c673e7d45bdecfdd677194
Ramaraju <buddharajuramaraju@gmail.com>
2018-01-28 22:38:51 +0530

    Assignment1.7

---
+++ Assignment1.7.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# ### Problem Statement
+# 
+# Write a Python program to reverse a word after accepting the input from the user.
+# Sample Output:
+# Input word: AcadGild
+# Output: dilGdacA
+
+# In[1]:
+
+
+input_word = input("plese enter a word to reverse it : ")
+print(input_word[::-1])
+
